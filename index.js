4914312d44d18957330c60fdb40745bce81bccd1
zeilsman1 <112158011+zeilsman1@users.noreply.github.com>
2022-09-13 15:35:24 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const {Server}=require('socket.io');
+const {createServer}=require('http')
+
+
+const app= express();
+const httpServer=createServer(app);
+const io=new Server(httpServer);
+
+
+const port = process.env.PORT || 3000;
+
+io.on("connection", (socket) => {
+    console.log(socket.id)
+});
+
+app.get('/',(req,res)=>{res.send("hello")})
+
+httpServer.listen(port);
